34459348c5965f4d7dd9c77192bffa716ffd8669
Pandorage <clement.pontoizeau@hotmail.fr>
2021-01-19 14:03:37 +0100

    tesst

--- js/game.js
+++ js/game.js
@@ -17,18 +17,24 @@ character.x = (canvas.width - character.xSize)/2;
 character.y = canvas.height - character.ySize - 10;
 
 let gyroscope = new Gyroscope({frequency: 60});
+let gyroValue = {
+    x: 0,
+    y: 0,
+    z: 0
+}
+gyroscope.addEventListener('reading', e => {
+    gyroValue.x += gyroscope.x
+    gyroValue.y += gyroscope.y
+    gyroValue.z += gyroscope.z
+});
 gyroscope.start();
-
-const debug = document.getElementById("debug");
 
 function gameLoop(){
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,canvas.width,canvas.height);
-
-    debug.innerHTML = "Gyro x : " + gyroscope.x;
     
     ctx.fillStyle = 'green';
-    ctx.fillRect(character.x + gyroscope.x * 10, character.y, character.xSize, character.ySize);
+    ctx.fillRect(character.x + gyroscope.x * 5, character.y, character.xSize, character.ySize);
 }
 
 setInterval(gameLoop, 1000/60)
